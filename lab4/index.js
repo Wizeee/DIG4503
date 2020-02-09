87132373d0658558f6408165515b651a456b27ad
Wize <42750511+Wizeee@users.noreply.github.com>
2020-02-08 19:57:43 -0500

    Final Lab 4 submission
    Final Lab 4 submission

---
+++ lab4/index.js
@@ -0,0 +1,53 @@
+//declaration of variables and connection information.
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const chalk = require("chalk");
+
+const pokemons = require("json-pokemon");
+const pokeDex = pokemons;
+
+App.get("/id/:id", (request, response) => {
+    let result = {"error" : "That Pokemon is currently not in our database, apologizes."};
+
+    pokeDex.forEach((value) => {
+        if(value.id == request.params.id) {
+            // console.log(request.path);
+            result = value;
+        }
+    });
+		//used to color content
+    if(result.error) {
+        console.log(chalk.red(request.path));
+    }		//used to color content
+    else {
+        console.log(chalk.green(req.path));
+    }
+
+    response.send(result);
+});
+
+App.get("/name/:name", (request, response) => {
+    let result = {"error" : "That Pokemon is currently not in our database, apologizes."};
+
+    pokeDex.forEach((value) => {
+        if(value.name.toLowerCase() == req.params.name) {
+            // console.log(req.path);
+            result = value;
+        }
+    });
+		//used to color content
+    if(result.error) {
+        console.log(chalk.red(request.path));
+    }
+    else { //used to color content
+        console.log(chalk.green(request.path));
+    }
+
+    response.send(result);
+});
+
+App.listen(port, function(){
+    console.log("Server running!");
+});
